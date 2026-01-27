6782c636d2790c1593a89175b2c4e072fffdf35f
Jazmin <jbedoya628@gmail.com>
2026-01-27 12:05:19 -0300

    ready

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,47 +1,45 @@
-import { useState, useContext } from "react"; // useState:maneja estados , useContext para usar el authContext y acceder al login
-import { AuthContext } from "../context/AuthContext"; // vive la logica de la autenticacion
+import { useState, useContext } from "react";
+import { AuthContext } from "../context/AuthContext";
 import { Link } from "react-router-dom";
 
-
-// Creamos el componente react llamado Login
 function Login() {
-  const { login } = useContext(AuthContext); // dame la funcion login del contexto, guarda el token y loguea al user
+  const { login } = useContext(AuthContext);
 
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
-  // funcion que se ejecuta cuando hace click en login
+  const API_URL = import.meta.env.VITE_API_URL;
+
   async function handleSubmit(e) {
-  e.preventDefault();
+    e.preventDefault();
 
-  const response = await fetch("http://127.0.0.1:5000/api/login", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json"
-    },
-    body: JSON.stringify({
-      email,
-      password
-    })
-  });
+    const response = await fetch(`${API_URL}/login`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        email,
+        password
+      })
+    });
 
-  if (!response.ok) {
-    alert("Email o contraseña incorrectos");
-    return;
+    if (!response.ok) {
+      alert("Email o contraseña incorrectos");
+      return;
+    }
+
+    const data = await response.json();
+    login(data.token);
   }
 
-  const data = await response.json();
-  login(data.token);
-}
-
-
-  // se ejecuta handleSubmit cuando presiona el boton
   return (
     <div className="login-container">
       <form className="login-box" onSubmit={handleSubmit}>
         <h2>Login</h2>
 
         <input
+          type="email"
           placeholder="Email"
           onChange={(e) => setEmail(e.target.value)}
         />
